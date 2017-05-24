843ee50649dd9ed795d4cd1b77687116840c9680
wuyongyang <495490892@qq.com>
2017-05-24 09:20:18 +0800

    add new

---
+++ ES6/profile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by asus on 2017/5/10.
+ */
+export var firstName = 'Michael';
+export var lastName = 'Jackson';
+export var year = 1958;
+
+// var firstName = 'Michael';
+// var lastName = 'Jackson';
+// var year = 1958;
+//
+// export {firstName, lastName, year};
---
+++ index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by asus on 2017/5/10.
+ */
